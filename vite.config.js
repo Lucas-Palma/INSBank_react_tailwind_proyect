a6006cfc85efd363662f30b8a43d5532346a86a3
Lucas-Palma <palmalucassantiago@gmail.com>
2023-03-16 13:27:17 -0300

    add: deploy 3rd try

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bank_modern_app/'
+  base: '/hoobank_react_tailwind_proyect/'
 })
