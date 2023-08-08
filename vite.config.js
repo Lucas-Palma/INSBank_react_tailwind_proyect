0a2c65f2c98d35c143238b3743d38584b200565a
Lucas-Palma <palmalucassantiago@gmail.com>
2023-08-08 15:33:40 -0300

    change vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/INSbank_react_tailwind_proyect/',
+  base: 'https://Lucas-Palma.github.io/INSbank_react_tailwind_proyect/',
 });
