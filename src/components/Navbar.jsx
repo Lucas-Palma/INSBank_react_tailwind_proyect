540e20dd85e4fb0ce3b5f8135cd80fbeb420b7b7
Lucas-Palma <palmalucassantiago@gmail.com>
2023-08-08 13:57:25 -0300

    feat: change navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import { close, logo, menu } from '../assets';
 import { navLinks } from '../constants';
 
@@ -7,48 +7,51 @@ const Navbar = () => {
 
   return (
     <nav className='w-full flex py-6 justify-between items-center navbar'>
-      <img src={logo} alt="hoobank" className='w-[124px] h-[32px]' />
-
+      <h1 className='text-[2rem] text-white font-poppins pl-2'>
+        BKY <span className=' text-secondary'>Bank</span>
+      </h1>
       <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
-            className={`font-poppins font-normal cursor-pointer text-[16px] hover:text-secondary ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white`}
+            className={`font-poppins font-normal cursor-pointer text-[16px] hover:text-secondary ${
+              index === navLinks.length - 1 ? 'mr-0' : 'mr-10'
+            } text-white`}
           >
-            <a href={`#${nav.id}`}>
-              {nav.title}
-            </a>
+            <a href={`#${nav.id}`}>{nav.title}</a>
           </li>
         ))}
       </ul>
 
       <div className='sm:hidden flex flex-1 justify-end items-center'>
-        <img 
-          src={toggle ? close : menu} 
-          alt="menu"
+        <img
+          src={toggle ? close : menu}
+          alt='menu'
           className='w-[28px] h-[28px] object-contain'
           onClick={() => setToggle((prev) => !prev)}
         />
 
-        <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar `}>
+        <div
+          className={`${
+            toggle ? 'flex' : 'hidden'
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar `}
+        >
           <ul className='list-none flex flex-col justify-end items-center flex-1'>
-          {navLinks.map((nav, index) => (
-            <li
-              key={nav.id}
-              className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'} text-white`}
-            >
-              <a href={`#${nav.id}`}>
-                {nav.title}
-              </a>
-            </li>
-          ))}
-        </ul>
-          
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] ${
+                  index === navLinks.length - 1 ? 'mr-0' : 'mb-4'
+                } text-white`}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
         </div>
-
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
